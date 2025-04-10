3e45a525acb31e2ab721c6fea8e436cf3e129249
Louis Potdevin <l.potdevin.informatique@gmail.com>
2025-04-10 16:46:47 +0200

    Réinitialisation de la migration

--- migrations/Version20250410144601.php
+++ migrations/Version20250410144601.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250410143902 extends AbstractMigration
+final class Version20250410144601 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,9 @@ final class Version20250410143902 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE images (id INT AUTO_INCREMENT NOT NULL, url VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, nb_download INT NOT NULL, nb_opened INT NOT NULL, last_time_viewed DATETIME NOT NULL, is_deleted TINYINT(1) NOT NULL, creation_date DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
         $this->addSql(<<<'SQL'
             CREATE TABLE logs (id INT AUTO_INCREMENT NOT NULL, images_id INT NOT NULL, date DATETIME NOT NULL, INDEX IDX_F08FC65CD44F05E5 (images_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
@@ -33,6 +36,9 @@ final class Version20250410143902 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
             ALTER TABLE logs DROP FOREIGN KEY FK_F08FC65CD44F05E5
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE images
         SQL);
         $this->addSql(<<<'SQL'
             DROP TABLE logs
