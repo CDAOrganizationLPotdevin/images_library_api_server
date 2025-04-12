03cfa1357c38f7a480a5fdbb74041f01a335b1f1
Lenny Moinardeau <moinardeaulenny2004@gmail.com>
2025-04-12 14:41:37 +0200

    sauvegarde intermédiaire

--- migrations/Version20250410145722.php
+++ migrations/Version20250410145722.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250410144601 extends AbstractMigration
+final class Version20250410145722 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -25,6 +25,9 @@ final class Version20250410144601 extends AbstractMigration
         SQL);
         $this->addSql(<<<'SQL'
             CREATE TABLE logs (id INT AUTO_INCREMENT NOT NULL, images_id INT NOT NULL, date DATETIME NOT NULL, INDEX IDX_F08FC65CD44F05E5 (images_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', available_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', delivered_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE logs ADD CONSTRAINT FK_F08FC65CD44F05E5 FOREIGN KEY (images_id) REFERENCES images (id)
@@ -42,6 +45,9 @@ final class Version20250410144601 extends AbstractMigration
         SQL);
         $this->addSql(<<<'SQL'
             DROP TABLE logs
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE messenger_messages
         SQL);
     }
 }
--- src/Controller/ImageUploaderController.php
+++ src/Controller/ImageUploaderController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use DateTime;
+use DateTimeZone;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -10,26 +13,44 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ImageUploaderController extends AbstractController
 {
-    
+
     #[Route('/upload/images', name: 'image_upload', methods: ['POST'])]
     public function upload(Request $request): Response
     {
-        
+
         $image = $request->files->get('image'); // Récupérer le fichier
-        
+        $name = $image->getClientOriginalName();
+
         if ($image) {
 
             $destination = $this->getParameter('kernel.project_dir') . '/public/images';
-            $newFilename = uniqid() . '.' . $image->guessExtension();
+            $newFilename = $name . "_" . uniqid() . '.' . $image->guessExtension();
             $image->move($destination, $newFilename);
 
-            return new Response('Image downloaded successfully', Response::HTTP_OK);
+            $client = HttpClient::create();
+            $response = $client->request('POST', 'http://127.0.0.1:8002/images', [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                ],
+                'json' => [
+                    "url" => "http://127.0.0.1:8002/images/" . $newFilename,
+                    "name" => $name,
+                    "nb_download" => 0,
+                    "nb_opened" => 0,
+                    "last_time_viewed" => new DateTime(), // Date actuelle
+                    "is_deleted" => false,
+                    "creation_date" => new DateTime(), // Date actuelle
+                    "logs" => []
+                ],
+            ]);
 
+
+            return new Response('Image downloaded successfully', Response::HTTP_OK);
         }
-        
+
         return new Response('No image uploaded in api', Response::HTTP_BAD_REQUEST);
     }
-    
 
-    
+
+
 }
